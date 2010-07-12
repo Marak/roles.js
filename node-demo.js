8711d26d2a7542e6f4acce2639ca076f38e2df31
Marak Squires <marak.squires@gmail.com>
2010-07-12 03:49:54 -0400

    adding core api methods

--- node-demo.js
+++ node-demo.js
@@ -6,7 +6,13 @@ roles = require('./lib/roles');
 // load up some example roles for basic role management on resources
 fs.readFile('./lib/exampleRoles.json', function (err,data) {
   
+  // parse the exampleRoles.json file into a JSON object
   roles._data = JSON.parse(data.toString());
+  
+  // NOTE : it would be pretty easy to create some API syntax sugar in the library
+  //        so you can only have one or two calls instead of the following seqeuence
+  //        
+  //        if anyone wants to create some i'll pull the changes, if not it will be next release or so
   
   // add a user
   roles.addUser('Bob');
@@ -17,6 +23,16 @@ fs.readFile('./lib/exampleRoles.json', function (err,data) {
   // add group
   roles.addGroup('Fly Fishers');
 
+  // add user to group
+  roles.addUserToGroup('Bob', 'Fly Fishers');
+
+  // add user to group
+  roles.addRoleToGroup('is allowed to go fly fishing', 'Fly Fishers');
+
+  // inherit roles from group, in this case we give "Fly Fishers" all the same roles as "Guests"
+  roles.inheritRolesFromGroup('Fly Fishers', 'Guests');
+  
+  
   //sys.puts('hello');
   //sys.puts(JSON.stringify(roles));
 
