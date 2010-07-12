24870fa75fdc87e0c22525244fe5eccb5d9b6502
Marak Squires <marak.squires@gmail.com>
2010-07-12 04:23:21 -0400

    added more api methods. add 'can' method to string prototype

--- node-demo.js
+++ node-demo.js
@@ -30,11 +30,36 @@ fs.readFile('./lib/exampleRoles.json', function (err,data) {
   roles.addRoleToGroup('is allowed to go fly fishing', 'Fly Fishers');
 
   // inherit roles from group, in this case we give "Fly Fishers" all the same roles as "Guests"
-  roles.inheritRolesFromGroup('Fly Fishers', 'Guests');
+  //roles.inheritRolesFromGroup('Fly Fishers', 'Guests');
   
   
-  //sys.puts('hello');
-  //sys.puts(JSON.stringify(roles));
+  // now that we have loaded some roles and created some new ones, lets try some test cases
+  
+  // first, lets see all the current groups and their roles
+  var theGroups = roles.getGroups();
+  //sys.puts(JSON.stringify(theGroups, true, 1));
 
+  // okay that was interesting, but kinda hard to read. lets try a basic permissions check
+  
+  
+  if("Marak".can('edit a resource')){
+    sys.puts('Marak can "edit a resource" because he is in the "Administrators" group, which inherits the "Moderators" group, which can edit resources.')
+  }
+  
+  var someuser = "Bob";
+  
+  if(someuser.can('edit a resource')){
+    // i don't think so Bob!
+  }
+  else{
+    sys.puts('Bob cannot "edit a resource" because he is not part of any group that has this role.')
+  }
+  
+  
+  
+  
+  sys.puts();
+  
+  
 });
 
